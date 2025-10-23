e5e9c1801aed62e70019994cddc288992d562bc5
Rahul <rahul.mymail1@gmail.com>
2025-10-23 20:56:52 +0530

    feat: swagger init

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+import express, { Response} from 'express';
 import cors from "cors"
 import { errorMiddleware } from '../../../packages/error-handler/error-middlware';
 import cookieParser from 'cookie-parser';
 import router from './routes/auth.routes';
+import swaggerUi from "swagger-ui-express";
+
+const swaggerDocument = require("./swagger-output.json")
 
 const app = express();
 
@@ -15,18 +18,24 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
+app.get('/', (_, res: Response) => {
     res.send({ 'message': 'Hello from auth service'});
 });
 
-app.use("/api", router)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use("/docs-json", (_, res) => {
+    res.json(swaggerDocument)
+})
 
+app.use("/api", router)
 app.use(errorMiddleware);
 
-const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 const server = app.listen(port, () => {
     console.log(`Auth-Service running at http://localhost:${port}/api`);
+    console.log(`Docs available at http://localhost:${port}/docs`)
 });
+
 
 server.on("error", (error) => {console.log("Server Error", error)})
